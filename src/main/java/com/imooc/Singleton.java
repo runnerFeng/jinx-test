9ba63440e780a4e3ceb48a27ceef2fb2afa832f6
Feng <liufeng@kuanter.com>
2017-01-24 17:54:04 +0800

    commit

---
+++ src/main/java/com/imooc/Singleton.java
@@ -0,0 +1,14 @@
+package com.imooc;
+
+/**
+ * Desc:
+ * Created by lf on 2017/1/23
+ */
+public class Singleton {
+        private Singleton(){
+        }
+    private static Singleton instance = new Singleton();
+    public static Singleton getInstance(){
+        return instance;
+    }
+}
---
+++ src/main/java/com/imooc/Test.java
@@ -0,0 +1,16 @@
+package com.imooc;
+
+/**
+ * Desc:
+ * Created by lf on 2017/1/24
+ */
+public class Test {
+    public static void main(String[] args) {
+//        Singleton s1 = new Singleton();
+//        Singleton s2 = new Singleton();
+//        System.out.println(s1 == s2);
+        Singleton s1 = Singleton.getInstance();
+        Singleton s2 = Singleton.getInstance();
+        System.out.println(s1== s2);
+    }
+}
