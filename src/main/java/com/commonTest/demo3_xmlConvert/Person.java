f7d9b44dc5c52b2ec1c8e26791b0230280c007a2
Feng <liufeng@kuanter.com>
2017-02-21 21:24:36 +0800

    xmlConvert

---
+++ src/main/java/com/commonTest/demo3_xmlConvert/Person.java
@@ -0,0 +1,47 @@
+package com.commonTest.demo3_xmlConvert;
+
+import com.thoughtworks.xstream.annotations.XStreamAlias;
+import com.thoughtworks.xstream.annotations.XStreamAsAttribute;
+import com.thoughtworks.xstream.annotations.XStreamImplicit;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Desc:
+ * Created by lf on 2017/2/21
+ */
+@XStreamAlias("person")
+public class Person {
+    //将name设置为XML person 元素的 attribute
+//    @XStreamAsAttribute()
+    private String name;
+    private int phoneNuber;
+    //将此字段名在XML中去掉
+    @XStreamImplicit()
+    private List<Address> addresses = new ArrayList<Address>();
+
+    public List<Address> getAddresses() {
+        return addresses;
+    }
+
+    public void setAddresses(List<Address> addresses) {
+        this.addresses = addresses;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPhoneNuber() {
+        return phoneNuber;
+    }
+
+    public void setPhoneNuber(int phoneNuber) {
+        this.phoneNuber = phoneNuber;
+    }
+}
