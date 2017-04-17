eef7f7fa29c69ec918dfd1a18cdbc7ce16c92c52
Feng <liufeng@kuanter.com>
2017-04-17 20:11:16 +0800

    1.first

--- src/main/java/com/commonTest/demo9_gitRollBack/gitTagRollbackTest.java
+++ src/main/java/com/commonTest/demo9_gitRollBack/gitTagRollbackTest.java
@@ -4,10 +4,8 @@ package com.commonTest.demo9_gitRollBack;
  * Desc:
  * Created by lf on 2017/4/17
  */
-public class GitRollbackTest {
-
+public class gitTagRollbackTest {
     public static void main(String[] args) {
-        System.out.println("这是第一次提交！");
-
+        System.out.println("first!");
     }
 }
