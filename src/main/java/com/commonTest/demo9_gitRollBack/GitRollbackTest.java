57f68999cc93f044d1cdff709941812d6f3d75ee
Feng <liufeng@kuanter.com>
2017-04-17 13:04:11 +0800

    1.第三次，reset测试

--- src/main/java/com/commonTest/demo9_gitRollBack/GitRollbackTest.java
+++ src/main/java/com/commonTest/demo9_gitRollBack/GitRollbackTest.java
@@ -1,5 +1,7 @@
 package com.commonTest.demo9_gitRollBack;
 
+import com.oracle.xmlns.internal.webservices.jaxws_databinding.SoapBindingUse;
+
 /**
  * Desc:
  * Created by lf on 2017/4/17
@@ -10,5 +12,7 @@ public class GitRollbackTest {
         System.out.println("这是第一次提交！");
 
         System.out.printf("这是第二次提交！");
+
+        System.out.println("这是第三次提交！");
     }
 }
