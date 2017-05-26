8e9b003c00effbc87a2095c6b4ead833107739c1
liufeng <631819061@qq.com>
2017-05-26 18:02:42 +0800

    commit

--- src/main/java/com/commonTest/demo2/Test.java
+++ src/main/java/com/commonTest/demo2/Test.java
@@ -50,12 +50,9 @@ public class Test {
         String jsonFromArr = mapper.writeValueAsString(strArr);
         System.out.println(jsonFromArr);
         Student[] stuArr = mapper.readValue(jsonFromArr, Student[].class);
-        System.out.println(stuArr);
+        System.out.println(stuArr);System.out.println(stuArr);
         System.out.println((String) mapper.readValue(jsonFromArr, new TypeReference<List<Student>>() {
         }));
-
-
-
 
     }
 }
