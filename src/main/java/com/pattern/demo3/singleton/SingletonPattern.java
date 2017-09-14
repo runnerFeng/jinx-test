1e5e2457fcd0f035af8da38bf5014eebe07e2c0c
liufeng <631819061@qq.com>
2017-09-14 16:36:36 +0800

    singleton patern final关键字分析

--- src/main/java/com/pattern/demo3/singleton/SingletonPattern.java
+++ src/main/java/com/pattern/demo3/singleton/SingletonPattern.java
@@ -6,17 +6,18 @@ package com.pattern.demo3.singleton;
  */
 public class SingletonPattern {
 
-    //此处final具体几个意思？要不要都行吗？
-    private static final SingletonPattern SINGLETON_PATTERN = new SingletonPattern();
+    //final作用，阻止在getInstance方法中修改SINGLETON_PATTERN
+    public static final SingletonPattern SINGLETON_PATTERN = new SingletonPattern();
 
-    //private修饰，限制外界不能直接产生一个实例
+    //private修饰，限制住不能直接产生一个实例
     private SingletonPattern() {
     }
 
     //添加synchronized关键字是防止多线程环境下的问题
     public synchronized static SingletonPattern getInstance() {
+//        SingletonPattern singletonPattern= new SingletonPattern();
+//        return SINGLETON_PATTERN=singletonPattern;
         return SINGLETON_PATTERN;
     }
-
 
 }
