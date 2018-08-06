80a52c362c2aa4912a900863981410f0812ef84c
runnerFeng <631819061@qq.com>
2018-08-07 00:19:03 +0800

    迭代器模式

--- src/main/java/com/pattern/demo11/decorator/demo2/Father.java
+++ src/main/java/com/pattern/demo11/decorator/demo2/Father.java
@@ -2,15 +2,15 @@ package com.pattern.demo11.decorator.demo2;
 
 
 /**
- * Desc:
+ * Desc:引用子类对象的链式传递技巧
  * Created by jinx on 2017/8/25.
  */
 public class Father {
     public static void main(String[] args) {
         SchoolReport schoolReport = null;
         schoolReport = new ForthGradeSchooleReport();
-//        schoolReport.report();
 
+//        schoolReport.report();
         schoolReport = new HighScoreDecorator(schoolReport);
 
         schoolReport = new SortDecorator(schoolReport);
