1038b63a04da95ce14440929eb0828e6dd343c1d
runnerFeng <631819061@qq.com>
2018-08-31 01:17:57 +0800

    1.测试

---
+++ src/main/java/com/common/test/demo20/Test.java
@@ -0,0 +1,9 @@
+package com.common.test.demo20;
+
+/**
+ * @author jinx
+ * @date 2018/08/31 00:46
+ * Desc:
+ */
+public @interface Test {
+}
---
+++ src/main/java/com/common/test/demo31_annotation/AnnotationTest.java
@@ -0,0 +1,10 @@
+package com.common.test.demo31_annotation;
+
+/**
+ * @author jinx
+ * @date 2018/08/27 23:22
+ * Desc:
+ */
+public class AnnotationTest {
+
+}
