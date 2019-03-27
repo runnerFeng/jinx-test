4d36e3d73fb30faba5b3a3f3f85d656fca6e08f5
liufeng <631819061@qq.com>
2019-03-27 18:11:10 +0800

    1.第一次提交

---
+++ src/main/java/com/audition/demo3/GitTest.java
@@ -0,0 +1,12 @@
+package com.audition.demo3;
+
+/**
+ * @author jinx
+ * @date 2019/3/27 18:10
+ * Desc:
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println(1);
+    }
+}
