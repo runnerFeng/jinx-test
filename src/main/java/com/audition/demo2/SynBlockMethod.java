b81b90b819a64831f3c3fc578d4e2a09e8972f82
liufeng <631819061@qq.com>
2019-03-27 18:10:15 +0800

    1.完善注释

--- src/main/java/com/audition/demo2/SynBlockMethod.java
+++ src/main/java/com/audition/demo2/SynBlockMethod.java
@@ -6,8 +6,8 @@ package com.audition.demo2;
  * Desc:
  */
 public class SynBlockMethod {
-    public void testA(){
-        synchronized (new Object()){
+    public void testA() {
+        synchronized (new Object()) {
             System.out.println("测试同步代码块编译后的字节码");
         }
     }
