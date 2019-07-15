4c7dce18a6400977eb9fede715c8aeb7ea59479e
liufeng <liufeng@dae.org>
2019-07-15 20:15:48 +0800

    1.add java guide direct

---
+++ src/main/java/com/javaGuide/basis/Collections.java
@@ -0,0 +1,9 @@
+package com.javaGuide.basis;
+
+/**
+ * @Author: jinx
+ * @Date: 2019-07-15 19:54
+ * @Desc:
+ */
+public class Collections {
+}
---
+++ src/main/java/com/javaGuide/basis/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * @Author: jinx
+ * @Date: 2019-07-15 19:54
+ * @Desc:Collections&Arrays utils
+ */
+package com.javaGuide.basis;
