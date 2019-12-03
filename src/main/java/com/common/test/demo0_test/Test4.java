a3b4c5085e0331c27b801434046760d34b8debad
liufeng <liufeng@dae.org>
2019-12-03 20:02:39 +0800

    1.merge

---
+++ src/main/java/com/common/test/demo0_test/Test4.java
@@ -0,0 +1,21 @@
+package com.common.test.demo0_test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @Author: jinx
+ * @Date: 2019-09-26 16:56
+ * @Desc:
+ */
+public class Test4 {
+    public static void main(String[] args) {
+        List<String>  list = new ArrayList<>();
+        list.add("a");
+        list.add("b");
+        list.add("c");
+        list.add("d");
+        list.add("e");
+        System.out.println(list.contains("a"));
+    }
+}
