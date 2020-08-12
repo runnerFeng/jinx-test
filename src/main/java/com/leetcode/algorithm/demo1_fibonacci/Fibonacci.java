4061ee9cbe7d5b0aef6833bc58cb60366931c8c3
liufeng <liufeng@dae.org>
2020-08-12 13:52:11 +0800

    优化

--- src/main/java/com/leetcode/algorithm/demo1_fibonacci/Fibonacci.java
+++ src/main/java/com/leetcode/algorithm/demo1_fibonacci/Fibonacci.java
@@ -13,7 +13,7 @@ public class Fibonacci {
     }
 
     private static int fibonacci(int n) {
-        if (n < 1) {
+        if (n <= 1) {
             return n;
         }
         return fibonacci(n - 1) + fibonacci(n - 2);
