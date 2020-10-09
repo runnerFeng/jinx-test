3d0ebe4442a94cdbb2a5467d07b2856f578ce440
liufeng <liufeng@dae.org>
2020-10-09 23:10:59 +0800

    test

---
+++ src/main/java/com/audition/demo4/Test.java
@@ -0,0 +1,58 @@
+package com.audition.demo4;
+
+import java.awt.event.ActionEvent;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @Author: Aug
+ * @Date: 2020-10-09 22:02
+ * @Desc: 获取该元素在链表中可能出现的位置
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        Map map = new HashMap();
+
+        List list = new ArrayList<>();
+    }
+
+//    public int[] indexOf(int t) {
+//        Node n = head;
+//        int[] arrays = new int[n.length()];
+//        int j = 0;
+//        // 标记是否是否匹配到链表中的元素
+//        int flag = 0;
+//        for (int i = 0; n.next != null; i++) {
+//            n = n.next;
+//            if (n.item.equals(t)) {
+//                arrays[j] = i;
+//                flag = -1;
+//                j++;
+//            }
+//        }
+//        // 如果没有匹配到链表中的元素数组中只保存一个值-1
+//        if (flag == 0) {
+//            arrays[0] = -1;
+//        }
+//        return arrays;
+//    }
+//
+//    public void actionPerformed(ActionEvent e) {
+//        if (tf1.getText.length() == 0) {
+//            tf3.setText("请在第一栏中输入一个字符串");
+//        } else {
+//            int[] order = L.indexOf(tf1.getText());
+//            if (order[0] != -1) {
+//                tf3.setText("顺序表中出现该值的位置有:" + order);
+//            } else {
+//                tf3.setText("此顺序表中不包含值为" + tf1.getText() + "的数据元素");
+//            }
+//        }
+//        tf1.setText("");
+//        tf3.setText("");
+//    }
+
+}
---
+++ src/main/java/com/audition/demo4/Test2.java
@@ -0,0 +1,17 @@
+package com.audition.demo4;
+
+/**
+ * @Author: Aug
+ * @Date: 2020-10-09 22:11
+ * @Desc:
+ */
+public class Test2 {
+
+    public static void main(String[] args) {
+        int[] arrays = new int[10];
+        System.out.println(arrays.length);
+        System.out.println(arrays[0]);
+        System.out.println(arrays[1]);
+        System.out.println(arrays[2]);
+    }
+}
