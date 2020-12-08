8e6ac1d42f400c4729cf5f813e99eb66b15e4d8e
liufeng <liufeng@dae.org>
2020-12-08 10:51:40 +0800

    select insert

--- src/main/java/com/leetcode/algorithm/chacpter1/demo2/method1/SelectInsect.java
+++ src/main/java/com/leetcode/algorithm/chacpter1/demo2/method1/SelectInsect.java
@@ -1,9 +1,10 @@
-package com.leetcode.algorithm.chacpter1.demo2;
+package com.leetcode.algorithm.chacpter1.demo2.method1;
 
 /**
  * @Author: Aug
  * @Date: 2020-08-17 21:24
- * @Desc:搜索插入位置
+ * @Desc:搜索插入位置:二分法
+ * 时间复杂度：O(logN)
  */
 public class SelectInsect {
 
---
+++ src/main/java/com/leetcode/algorithm/chacpter1/demo2/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * @Author: Aug
+ * @Date: 2020-12-07 19:20
+ * @Desc:
+ */
+package com.leetcode.algorithm.chacpter1.demo2;
+/**
+ * 搜索插入位置
+ */
