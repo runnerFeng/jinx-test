55df62137b25adf4cb39a90bf5089da06249b302
liufeng <liufeng@dae.org>
2020-12-22 20:12:17 +0800

    remove duplicates

---
+++ src/main/java/com/leetcode/algorithm/chacpter2/demo2_removeDuplicates/method1/RemoveDuplicates.java
@@ -0,0 +1,26 @@
+package com.leetcode.algorithm.chacpter2.demo2_removeDuplicates.method1;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @Author: Aug
+ * @Date: 2020-12-22 19:51
+ * @Desc:
+ */
+@Slf4j
+public class RemoveDuplicates {
+
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 2, 3, 3, 4, 6, 8, 8, 9};
+        int len = removeDuplicates(nums);
+        log.info("nums len:{}", len);
+    }
+
+    private static int removeDuplicates(int[] nums) {
+        int i , j ,result = 0;
+        if (nums.length<=2){
+
+        }
+        return 0;
+    }
+}
---
+++ src/main/java/com/leetcode/algorithm/chacpter2/demo2_removeDuplicates/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * @Author: Aug
+ * @Date: 2020-12-22 19:51
+ * @Desc:删除数组中的重复项：给定一个排序数组，你需要在原地删除出现的元素，使得每个元素只出现一次，返回移除后数组的长度。不要使用额外的数组
+ * 空间，你必须在原地修改输入数组，并在使用O(1)额外空间的情况下完成。
+ */
+package com.leetcode.algorithm.chacpter2.demo2_removeDuplicates;
