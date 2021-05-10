1eed3383c034f20016c0231dabfac7c776f65903
runnerFeng <631819061@qq.com>
2021-05-10 23:17:17 +0800

    '实现readResolve（）方法阻止反序列化破坏单例模式'

--- src/main/java/com/common/test/demo41_singleton/method1/Singleton.java
+++ src/main/java/com/common/test/demo41_singleton/method1/Singleton.java
@@ -1,11 +1,13 @@
 package com.common.test.demo41_singleton.method1;
 
+import java.io.Serializable;
+
 /**
  * @Author: Aug
  * @Date: 2021-03-11 10:54
- * @Desc: 基于volatile的双重检查锁定
+ * @Desc: 基于volatile的双重检查锁定,但是这种方式在反序列化时也会通过反射重新破坏单例模式，可以通过重写readResolve（）方法来阻止这种破坏
  */
-public class Singleton {
+public class Singleton implements Serializable {
 
     private static volatile Singleton instance;
 
@@ -19,5 +21,10 @@ public class Singleton {
         }
         return instance;
     }
+
+    private Object readResolve(){
+        return instance;
+    }
+
 
 }
