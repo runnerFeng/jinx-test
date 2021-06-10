da7f520a37d78a3b18aa84538b088989dbf133b4
liufeng <liufeng@dae.org>
2021-06-10 17:27:59 +0800

    final parameter test

--- src/main/java/com/common/test/demo47_finalParameter/Test.java
+++ src/main/java/com/common/test/demo47_finalParameter/Test.java
@@ -5,7 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 /**
  * @Author: fans
  * @Date: 2021/6/10 02:06
- * @Desc:
+ * @Desc: 对于基本类型不可变指值不可变，对于引用类型不可变指引用地址不可变，但引用对象的属性值可以改变
  */
 @Slf4j
 public class Test {
@@ -14,14 +14,14 @@ public class Test {
         Student student = new Student();
         student.setUsername("jinx");
         student.setAge(10);
-        log.info("username:{},age:{},student:{}", student.getUsername(), student.getAge(),student.hashCode());
-       change(student);
-        log.info("username:{},age:{},student:{}", student.getUsername(), student.getAge(),student.hashCode());
+        log.info("username:{},age:{},student:{}", student.getUsername(), student.getAge(), student.hashCode());
+        change(student);
+        log.info("username:{},age:{},student:{}", student.getUsername(), student.getAge(), student.hashCode());
 
     }
 
-    private static void change( Student student) {
-        student = new Student();
+    private static void change(final Student student) {
+//        student = new Student();
         student.setAge(11);
     }
 
