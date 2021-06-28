3acdfdb0e30817b557b590ecb0393ef641231ca5
liufeng <liufeng@dae.org>
2021-06-28 18:13:33 +0800

    mapstruct example

---
+++ src/main/java/com/common/test/demo49_mapStruct/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * @Author: Aug
+ * @Date: 2021-06-28 18:06
+ * @Desc: href=https://blog.csdn.net/weixin_30342639/article/details/104223046?utm_medium=distribute.pc_relevant.none-task-blog-2%7Edefault%7EBlogCommendFromMachineLearnPai2%7Edefault-2.control&depth_1-utm_source=distribute.pc_relevant.none-task-blog-2%7Edefault%7EBlogCommendFromMachineLearnPai2%7Edefault-2.control
+ */
+package com.common.test.demo49_mapStruct;
