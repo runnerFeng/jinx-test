116a040c82cb74bc2406850fcaeb4e11a5b443cc
liufeng <liufeng@dae.org>
2021-07-27 11:37:06 +0800

    #更新排序顺序

--- src/main/java/com/algorithm/sortTest/insertSort/InsertSort.java
+++ src/main/java/com/algorithm/sortTest/insertSort/InsertSort.java
@@ -20,7 +20,7 @@ public class InsertSort {
         // 这种方式无效交换次数较多
         for (int i = 1; i < array.length; i++) {
             for (int j = i; j > 0; j--) {
-                if (array[j] > array[j - 1]) {
+                if (array[j] < array[j - 1]) {
                     int temp = array[j];
                     array[j] = array[j - 1];
                     array[j - 1] = temp;
