b5d0bc0e9e8ce73941ad798ec88f067feafd50b0
liufeng <liufeng@xt.com>
2024-07-10 23:29:08 +0800

    update2

--- src/main/java/com/leetcode/algorithm/chacpter1/demo1_fibonacci/method2/Fibonacci.java
+++ src/main/java/com/leetcode/algorithm/chacpter1/demo1_fibonacci/method2/Fibonacci.java
@@ -32,7 +32,6 @@ public class Fibonacci {
         cache[1] = 1;
         for (int i = 2; i <= n; i++) {
             cache[i] = cache[i - 2] + cache[i - 1];
-
         }
         return cache[n];
     }
